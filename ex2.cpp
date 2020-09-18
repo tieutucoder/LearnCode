0f7fd2ae978cf2fcadf2cf2e505cc5e64b1aecc8
ohellobeyeu <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-18 16:38:55 +0700

    Add files via upload

---
+++ ex2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int canh;
+  cin >> canh;
+  cout << "\n";
+  for (int i=0; i<5; i++) {
+    for (int j=0; j<4; j++) {
+      if (j==0 || j==3 || i==0 || i==4) {
+        cout << canh;
+      } else {
+        cout << " ";
+      };
+    };
+    cout << "\n\n";
+  };
+  return 0;
+}
