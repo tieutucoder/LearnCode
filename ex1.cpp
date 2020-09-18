d5713238d2611e336f5eb0ffbb4bd75b15c0d769
ohellobeyeu <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-18 16:24:20 +0700

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int canh;
+  cout << "Input ";
+  cin >> canh;
+  cout << "Chu vi " << canh*4;
+  cout << "\nChu vi " << canh*canh;
+  return 0;
+}
