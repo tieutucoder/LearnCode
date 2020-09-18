c9f9c92176e6d965d4fa11b63f3fc67d198756a6
ohellobeyeu <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-18 17:23:09 +0700

    Add files via upload
    B5. Ghi các chữ số

---
+++ ex5.cpp
@@ -0,0 +1,14 @@
+﻿//Ghi các chữ số
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int songuyen,so1,so2,so3;
+  cin >> songuyen;
+  so1 = songuyen/100;
+  so2 = (songuyen-so1*100)/10;
+  so3 = (songuyen-so1*100)-so2*10;
+  cout << so1 << "\n" << so2 << "\n"<< so3;
+  return 0;
+}
