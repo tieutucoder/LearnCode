b550c3c5085f72a3a982068fa29e6088935ccd25
ohellobeyeu <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-18 17:34:02 +0700

    Update ex1.cpp

--- ex1.cpp
+++ ex1.cpp
@@ -5,9 +5,8 @@ using namespace std;
 int main ()
 {
   int canh;
-  cout << "Input ";
   cin >> canh;
   cout << "Chu vi " << canh*4;
-  cout << "\nChu vi " << canh*canh;
+  cout << "\nDien tich " << canh*canh;
   return 0;
 }
