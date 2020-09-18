fc626fe4cce9302938be88e2ecf441292a8811bc
ohellobeyeu <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-18 17:33:13 +0700

    Add files via upload
    B6. Tính toán

---
+++ ex6.cpp
@@ -0,0 +1,25 @@
+﻿//Tính toán
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int so1,so2,tong,hieu,tich,thuong;
+  cin >> so1 >> so2;
+  tong = so1+so2;
+  hieu = so1-so2;
+  tich = so1*so2;
+  if (so2 == 0) {
+    if (so1 > 0) {
+        cout << tong << "\n" << hieu << "\n" << tich << "\n" << "pos inf";
+    } else if (so1 < 0) {
+        cout << tong << "\n" << hieu << "\n" << tich << "\n" << "neg inf";
+    } else {
+      cout << tong << "\n" << hieu << "\n" << tich << "\n" << "null";
+    }
+  } else {
+    thuong = so1/so2;
+    cout << tong << "\n" << hieu << "\n" << tich << "\n" << thuong;
+  }
+  return 0;
+}
