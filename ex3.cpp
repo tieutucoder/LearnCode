ef91b0433364f05c461a7c14494143d135d17f26
ohellobeyeu <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-18 16:45:45 +0700

    Add files via upload
    "tà tữa"

---
+++ ex3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int x,y,gia_x,gia_y,sodu;
+  cin >> x >> y >> gia_x >> gia_y;
+  if (gia_x < 500 || gia_y <500) {
+    cout << "Menh gia nho nhat la 500d";
+  } else {
+    sodu = 500000 - gia_x*x - gia_y*y;
+    cout << "So tien ma ban Nguyen nhan lai " << sodu;
+  };
+  return 0;
+}
