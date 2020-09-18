edec253f0e34419165e2aba9191c09cf4a7bf5da
ohellobeyeu <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-18 17:13:37 +0700

    Add files via upload
    B4. Nhà băng

---
+++ ex4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int x,y,z,sotien;
+  cin >> x >> y >> z;
+  if (y < 12) {
+    sotien = (x * y * z/100/12) + x;
+  } else {
+    if (y % 12 == 0) {
+      sotien = (x * y/12 * z/100) + x;
+    } else {
+      sotien = (x * y/12 * z/100) + (x * y%12 * z/100/12) + x;
+    }
+  };
+  cout << "So tien " << sotien;
+  return 0;
+}
