ebe9133605844a2b1202bb6b502bdbcc8a3acfeb
Đoàn Ngọc Lãm <57290977+lamngok1201@users.noreply.github.com>
2020-09-19 10:14:40 +0700

    Add files via upload

---
+++ Findthebit_template.cpp
@@ -0,0 +1,17 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+class
+;
+,
+###End banned keyword*/
+#include <iostream>
+using namespace std;
+int main(){
+	int n, k;
+	cin >> n >> k;
+	cout << (
+//###INSERT CODE HERE -
+        )
+    ;
+}
---
+++ abs_template.cpp
@@ -0,0 +1,15 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+using
+if
+switch
+while
+abs
+for
+cmath
+stdlib
+math
+###End banned keyword*/
+#include <iostream>
+//###INSERT CODE HERE -
