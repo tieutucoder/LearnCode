9f958d576f2087d93c1f2bc06d84c5c3685b03d1
ducansuper <71486502+ducansuper@users.noreply.github.com>
2020-09-19 21:17:00 +0700

    Add files via upload

---
+++ thuegiatrigiatang.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a, b, tien, thue;
+	cin >> a >> b;
+	tien = a * b;
+	thue = tien * 0.1;
+	cout << tien << endl << thue;
+	return 0;
+
+}
