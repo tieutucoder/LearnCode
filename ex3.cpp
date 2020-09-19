8f314a2e477e004b7d058dff68a82c96d6bc327b
ohellobeyeu <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-19 08:33:17 +0700

    Update ex3.cpp

--- ex3.cpp
+++ ex3.cpp
@@ -6,12 +6,8 @@ int main ()
 {
   int x,y,gia_x,gia_y,sodu;
   cin >> x >> y >> gia_x >> gia_y;
-  if (gia_x < 500 || gia_y <500) {
-    cout << "Menh gia nho nhat la 500d";
-  } else {
-    sodu = 500000 - gia_x*x - gia_y*y;
-    cout << "So tien ma ban Nguyen nhan lai " << sodu;
-  };
+  sodu = 500000 - gia_x*x - gia_y*y;
+  cout << "So tien ma ban Nguyen nhan lai " << sodu;
   return 0;
 }
 
