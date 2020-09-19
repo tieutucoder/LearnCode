2cd49b69094c40c187291b88f48e2e1f8bcb00d0
Phạm Quốc Cường <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-19 09:59:09 +0700

    Update ex5.cpp

--- ex5.cpp
+++ ex5.cpp
@@ -1,14 +1,28 @@
-﻿//Ghi các chữ số
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+using
+;
+###End banned keyword*/
 #include <iostream>
+
 using namespace std;
 
-int main ()
+
+
+int main()
 {
-  int songuyen,so1,so2,so3;
-  cin >> songuyen;
-  so1 = songuyen/100;
-  so2 = (songuyen-so1*100)/10;
-  so3 = (songuyen-so1*100)-so2*10;
-  cout << so1 << "\n" << so2 << "\n"<< so3;
-  return 0;
+    cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    
+    int x;
+
+    cin >> x;
+    
+    cout << 
+      x/100 <<
+      "\n" << (x-(x/100)*100)/10 << "\n" <<
+      (x-(x/100)*100)-((x-(x/100)*100)/10)*10
+    << endl;
+    return 0;
 }
