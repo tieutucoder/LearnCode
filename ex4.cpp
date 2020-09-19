0b10dddacb4ae2c7a2a34885003089d9011d87f8
ohellobeyeu <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-19 08:38:23 +0700

    Update ex4.cpp

--- ex4.cpp
+++ ex4.cpp
@@ -5,15 +5,7 @@ int main ()
 {
   int x,y,z,sotien;
   cin >> x >> y >> z;
-  if (y < 12) {
-    sotien = (x * y * z/100/12) + x;
-  } else {
-    if (y % 12 == 0) {
-      sotien = (x * y/12 * z/100) + x;
-    } else {
-      sotien = (x * y/12 * z/100) + (x * y%12 * z/100/12) + x;
-    }
-  };
+  sotien = (x * y * z/100/12) + x;
   cout << "So tien " << sotien;
   return 0;
 }
