7908db0a10f6e88705f84b3a43cd0e8bd2450d0d
Phạm Quốc Cường <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-20 07:53:08 +0700

    Update DuongTron.cpp

--- DuongTron.cpp
+++ DuongTron.cpp
@@ -1,10 +1,28 @@
-﻿#include <iostream>
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+using
+;
+###End banned keyword*/
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <cmath>
+
 using namespace std;
 
-int main () {
-  int nam;
-  cin >> nam;
-  (nam <= 100) ? cout << 1 : ((nam%100==0) ? cout << nam/100 : cout << (nam/100)+1);
 
-  return 0;
+
+int main()
+{
+    cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    
+    double x;
+
+    cin >> x;
+    cout << setprecision(15)
+    << x*2*M_PI << endl << x*x*M_PI
+    << endl;
+    return 0;
 }
