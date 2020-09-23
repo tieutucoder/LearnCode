c95aef3738a901b168a80679a309d496e8bbd481
Tung Le <tunglexuan2002@gmail.com>
2020-09-23 09:59:36 +0700

    update

--- baiTapTuan1/gocPhanTu.cpp
+++ baiTapTuan1/gocPhanTu.cpp
@@ -4,14 +4,16 @@ int main()
 {
 
     int goc, temp;
+
     cin >> goc;
     temp = goc / 360;
 
     (goc <= 360)?(temp = 0):(goc=goc - (360 * temp));
 
-    (goc <= 90)?(cout << 4):((90 < goc && goc <=180)?(cout << 3):((180 < goc && goc <=270)?(cout << 2):(cout <<"1")));
+    (0 < goc && goc <= 90)?(cout << 4):((90 < goc && goc <=180)?(cout << 3):((180 < goc && goc <=270)?(cout << 2):(cout <<"1")));
     //(90 < goc && goc <=180)?(cout << 3):(cout <<"");
     //(180 < goc && goc <=270)?(cout << 2):(cout <<"");
     //(270 < goc && goc <=360)?(cout <<1):(cout <<"");
+
     return 0;
 }
