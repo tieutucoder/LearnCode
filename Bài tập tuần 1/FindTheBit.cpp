72a66303e6d188dd0402a0b4852cbee1797474bd
Phạm Quốc Cường <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-23 09:25:06 +0700

    Find the bit

---
+++ Bài tập tuần 1/FindTheBit.cpp
@@ -0,0 +1,14 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+class
+;
+,
+###End banned keyword*/
+#include <iostream>
+using namespace std;
+int main(){
+	int n, k;
+	cin >> n >> k;
+  cout << (1 & (n >> k)) ;
+}
