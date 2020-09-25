698988c4910f7a795560fe5987ab2911cbdc7a11
Phạm Quốc Cường <44998768+ohellobeyeu@users.noreply.github.com>
2020-09-25 23:26:53 +0700

    Add files via upload

---
+++ Bài tập tuần 2/HonSo.cpp
@@ -0,0 +1,38 @@
+﻿/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+class
+###End banned keyword*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a;
+    int n;
+    cin >> a >> n;
+    double mang[n];
+    double b, min = a - int(a);
+
+    for(int i=0; i<n; i++) {
+      mang[i] = (double)i/n;
+      if (a - int(a) - mang[i] < 0) {
+        if ((a - int(a) - mang[i])*-1 < min) {
+          min = a - int(a) - mang[i];
+          b = mang[i];
+        }
+      } else {
+        if (a - int(a) - mang[i] < min) {
+          min = a - int(a) - mang[i];
+          b = mang[i];
+        }
+      };
+    }
+    cout << int(a);            
+    for (int j=0; j<n; j++) {
+      if ((double)j/n == b) {
+        cout << " " << j << "/" << n;
+      }
+    };
+}
---
+++ Bài tập tuần 2/NamNhuan.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+  unsigned int nam, count;
+  cin >> nam;
+  count = nam;
+  
+  if ((!(nam%100==0) && nam%4==0) || (nam%100==0 && nam%400==0)) {
+    cout << 0;
+  } else {
+    loop:
+      if ((!(count%100==0) && count%4==0) || (count%100==0 && count%400==0)) {
+        cout << count - nam;
+      } else {
+        count++;
+        goto loop;
+      };
+  };
+}
