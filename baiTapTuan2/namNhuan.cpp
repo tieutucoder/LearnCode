b426396aafb6ef4a3508e54a52bc1282d76fdcfb
Tung Le <tunglexuan2002@gmail.com>
2020-09-26 11:57:09 +0700

    update

--- baiTapTuan2/namNhuan.cpp
+++ baiTapTuan2/namNhuan.cpp
@@ -6,14 +6,26 @@ int main()
     int year;
     cin >> year;
     if (year < 0){
-         if (year % 4 == -1 || year == 1) {
-        cout << 1;
+         if (year % 4 == -1 || year == -1) {
+            if (((year+1)%100==0 && year%400!=0)&& year!=(-1)){
+                    cout << 5;
+            } else {
+                    cout <<1;
+            }
         }
-        else if (year % 4 == -2 || year == 2) {
-            cout << 2;
+        else if (year % 4 == -2 || year == -2) {
+            if (((year+2)%100==0 && year%400!=0)&& year!=(-2)){
+                    cout << 6;
+            } else {
+                    cout <<2;
+            }
         }
-        else if (year % 4 == -3 || year == 3) {
-            cout << 3;
+        else if (year % 4 == -3 || year == -3) {
+            if (((year+3)%100==0 && year%400!=0)&& year!=(-3)){
+                    cout << 7;
+            } else {
+                    cout <<3;
+            }
         }  else {
             if ((year%100==0 && year%400!=0)){
                 cout << 4;
@@ -23,13 +35,25 @@ int main()
         }
     } else {
         if (year % 4 == 1 || year == 1) {
-        cout << 3;
+            if ((year+3)%100==0 && year%400!=0){
+                    cout << 7;
+            } else {
+                    cout <<3;
+            }
         }
         else if (year % 4 == 2 || year == 2) {
-            cout << 2;
+             if ((year+2)%100==0 && year%400!=0){
+                    cout << 6;
+            } else {
+                    cout <<2;
+            }
         }
         else if (year % 4 == 3 || year == 3) {
-            cout << 1;
+             if ((year+1)%100==0 && year%400!=0){
+                    cout << 5;
+            } else {
+                    cout <<1;
+            }
         }
         else {
             if ((year%100==0 && year%400!=0)){
@@ -41,4 +65,3 @@ int main()
     }
     return 0;
 }
-
