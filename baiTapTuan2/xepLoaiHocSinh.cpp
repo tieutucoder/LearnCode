65c88769e04526dc25f577a0faccd575405ab953
Tung Le <tunglexuan2002@gmail.com>
2020-09-26 11:17:51 +0700

    Update

---
+++ baiTapTuan2/xepLoaiHocSinh.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Average;
+    float Math, Physics, Chemistry;
+    cin >> Math >> Physics >> Chemistry;
+    Average = (Math + Physics + Chemistry)*100 / 3;
+    cout << "DTB = " << Average/100 << "." <<Average%100 <<endl;
+    if(Average >= 900) {
+        cout << "Loai:XUAT SAC";
+    } else if(Average>=800){
+        cout << "Loai:GIOI";
+    } else if(Average>=700){
+        cout << "Loai:KHA";
+    } else if(Average>=600){
+        cout << "Loai:TB KHA";
+    } else if(Average>=500){
+        cout << "Loai:TB";
+    } else if(Average>=400){
+        cout << "Loai:YEU";
+    } else {
+        cout << "Loai:KEM";
+    }
+    return 0;
+}
+
+
