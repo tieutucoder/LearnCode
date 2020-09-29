7d4422a13005f21960486bcb6388aaa6112b465a
Tung Le <tunglexuan2002@gmail.com>
2020-09-29 13:12:41 +0700

    Update

--- baiTapTuan2/honSo.cpp
+++ baiTapTuan2/honSo.cpp
@@ -12,9 +12,7 @@ int main()
     double a;
     int n;
     std::cin >> a >> n;
-	/*
     std::cout << (int(a*n)/n) <<" ";
-
     if ((float(a*n)-int(a*n)) < 0.5) {
         if (((int(a*n)/float(n)-int(a*n)/n)*n)!=0) {
             std::cout << (int(a*n)/float(n)-int(a*n)/n)*n<<"/" <<n;
@@ -29,5 +27,5 @@ int main()
     //a = int(a*n)/float(n);
     //std::cout << a;
     //std::cout << (int(a*n)/float(n)-int(a*n)/n)*n+1 << "/" <<n;
-	*/
+
 }
