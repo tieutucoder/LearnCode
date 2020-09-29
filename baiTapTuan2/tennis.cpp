bf5de6b41b5828faf511fbf5022637bd0ec1d9ff
Tung Le <tunglexuan2002@gmail.com>
2020-09-29 13:09:49 +0700

    Update

---
+++ baiTapTuan2/tennis.cpp
@@ -0,0 +1,25 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+using
+if
+switch
+;
+?
+###End banned keyword*/
+#include <iostream>
+
+int main () {
+    unsigned int a, b;
+    std::cin >> a >> b ;
+
+
+
+    std::cout <<
+
+    (((a==6&&b<=4)||(a<=4&&b==6))||((a>4&&(b-a)==2)||(b>4&&(a-b)==2)))
+
+
+    << std::endl
+    ;
+}
