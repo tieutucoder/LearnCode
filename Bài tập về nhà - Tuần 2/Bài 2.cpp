bda2c332cfa729cab4c0a30dfa793b0a2d9b8a27
Phạm Quốc Cường <44998768+ohellobeyeu@users.noreply.github.com>
2020-10-18 04:39:28 +0700

    Update Bài 2.cpp

--- Bài tập về nhà - Tuần 2/Bài 2.cpp
+++ Bài tập về nhà - Tuần 2/Bài 2.cpp
@@ -1,76 +1,33 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main() {
 	int nam, sodu; cin >> nam;
-	if (nam > 0) {
-		switch ((nam % 60) % 12) {
-			case 0: cout << "THAN"; break;
-			case 1: cout << "DAU"; break;
-			case 2: cout << "TUAT"; break;
-			case 3: cout << "HOI"; break;
-			case 4: cout << "TY'"; break;
-			case 5: cout << "SUU"; break;
-			case 6: cout << "DAN"; break;
-			case 7: cout << "MAO"; break;
-			case 8: cout << "THIN"; break;
-			case 9: cout << "TY."; break;
-			case 10: cout << "NGO"; break;
-			case 11: cout << "MUI"; break;
-		}
-	}
+	char CHI[][5] = { "TY'", "SUU", "DAN", "MAO", "THIN", "TY.", "NGO", "MUI", "THAN", "DAU", "TUAT", "HOI" };
+	if (nam >= 0) cout << CHI[(nam + 8) % 12];
 	else {
-		nam = nam * -1;
-		(nam / 1000 == 0) ? sodu = (nam / 100) % 3 : sodu = ((nam / 1000) + (nam - 1000) % 100) % 3;
-		(nam % 100 > 12) ? nam = (nam % 100) % 12 : nam = nam;
-		switch (sodu)
+		nam = abs(nam);
+		(nam / 1000 == 0) ? sodu = (nam / 100) % 3 : sodu = ((abs(nam) / 1000) + abs(nam - 1000 * (abs(nam) / 1000))) % 3;
+		(abs(nam) % 100 > 12) ? nam = (abs(nam) % 100) % 12 : nam = abs(nam);
+		switch (sodu) {
+		case 0: {
+			char CHI[][5] = { "DAU", "THAN", "MUI", "NGO", "TY.", "THIN", "MAO", "DAN", "SUU", "TY'", "HOI", "TUAT" };
+			cout << CHI[abs(nam) % 100];
+			break;
+		}
+		case 1: {
+			char CHI[][5] = { "TY.", "THIN", "MAO", "DAN", "SUU", "TY'", "HOI", "TUAT", "DAU", "THAN", "MUI", "NGO" };
+			cout << CHI[abs(nam) % 100];
+			break;
+		}
+		case 2:
 		{
-		case 0: switch (nam % 100)
-		{
-			case 0: cout << "DAU"; break;
-			case 1: cout << "THAN"; break;
-			case 2: cout << "MUI"; break;
-			case 3: cout << "NGO"; break;
-			case 4: cout << "TY."; break;
-			case 5: cout << "THIN"; break;
-			case 6: cout << "MAO"; break;
-			case 7: cout << "DAN"; break;
-			case 8: cout << "SUU"; break;
-			case 9: cout << "TY'"; break;
-			case 10: cout << "HOI"; break;
-			case 11: cout << "TUAT"; break;
-		} break;
-		case 1: switch (nam % 100)
-		{
-			case 0: cout << "TY."; break;
-			case 1: cout << "THIN"; break;
-			case 2: cout << "MAO"; break;
-			case 3: cout << "DAN"; break;
-			case 4: cout << "SUU"; break;
-			case 5: cout << "TY'"; break;
-			case 6: cout << "HOI"; break;
-			case 7: cout << "TUAT"; break;
-			case 8: cout << "DAU"; break;
-			case 9: cout << "THAN"; break;
-			case 10: cout << "MUI"; break;
-			case 11: cout << "NGO"; break;
-		} break;
-		case 2: switch (nam % 100)
-		{
-			case 0: cout << "SUU"; break;
-			case 1: cout << "TY'"; break;
-			case 2: cout << "HOI"; break;
-			case 3: cout << "TUAT"; break;
-			case 4: cout << "DAU"; break;
-			case 5: cout << "THAN'"; break;
-			case 6: cout << "MUI"; break;
-			case 7: cout << "NGO"; break;
-			case 8: cout << "TY."; break;
-			case 9: cout << "THIN"; break;
-			case 10: cout << "MAO"; break;
-			case 11: cout << "DAN"; break;
-		} break;
+			char CHI[][5] = { "SUU", "TY", "HOI", "TUAT", "DAU", "THAN", "MUI", "NGO", "TY.", "THIN", "MAO", "DAN" };
+			cout << CHI[abs(nam) % 100];
+			break;
+		}
 		}
 	}
 }
