17c2fe4321f45c16f54c358f25593f0afb80a3f2
Tung Le <45787941+coderaohong@users.noreply.github.com>
2020-10-17 17:41:33 +0700

    Add files via upload

---
+++ baiTapTuan2/phuongTrinhBac2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int a, b, c;
+	cin >> a >> b >> c;
+	int delta;
+	delta = (b*b) - (4*a*c);
+	if(delta > 0) {
+		float no1, no2;
+		no1 = ((-b)+sqrt(delta)) / (2*a);
+		no2 = ((-b)-sqrt(delta)) / (2*a);
+		(no1>no2)?(cout <<"{"<<no2<<", "<<no1<<"}"):(cout <<"{"<<no1<<", "<<no2<<"}");
+	} else if(delta==0) {
+		cout << "{"<<-b / (2*a)<<"}";
+	} else {
+		cout <<"Ø";
+	}
+	return 0;
+}
