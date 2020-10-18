36792136df97210ac7143f9d25911551025efbc1
Tung Le <tunglexuan2002@gmail.com>
2020-10-18 22:24:06 +0700

    Update Phuong trinh bac 2

--- baiTapTuan2/phuongTrinhBac2.cpp
+++ baiTapTuan2/phuongTrinhBac2.cpp
@@ -5,14 +5,16 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	int a, b, c;
+	float a, b, c;
 	cin >> a >> b >> c;
-	int delta;
+	float delta;
 	delta = (b*b) - (4*a*c);
 	if(delta > 0) {
 		float no1, no2;
 		no1 = ((-b)+sqrt(delta)) / (2*a);
 		no2 = ((-b)-sqrt(delta)) / (2*a);
+		(no1 == (-0))?(no1 = 0):(no1 = no1);
+		(no2 == (-0))?(no2 = 0):(no2 = no2);
 		(no1>no2)?(cout <<"{"<<no2<<", "<<no1<<"}"):(cout <<"{"<<no1<<", "<<no2<<"}");
 	} else if(delta==0) {
 		cout << "{"<<-b / (2*a)<<"}";
