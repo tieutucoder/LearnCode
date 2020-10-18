1dea731577dd1d5a72588d4aaeba53dc74ea7c0f
dangtapcode <71515687+duy1110@users.noreply.github.com>
2020-10-18 10:17:26 +0700

    Add files via upload

---
+++ duongtron.cpp
@@ -0,0 +1,27 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+using
+;
+###End banned keyword*/
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <cmath>
+
+using namespace std;
+
+
+
+int main()
+{
+    cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+
+    double x;
+
+
+    cin >> x;
+    cout << setprecision(15)<<x*2*M_PI<<endl<<x*x*M_PI<<endl;
+    return 0;
+}    
