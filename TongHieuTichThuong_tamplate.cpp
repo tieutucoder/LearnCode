ce70eedd49edb92538595c91baa140f4d652a6ea
bichtuyenn <71558183+bichtuyenn@users.noreply.github.com>
2020-10-18 10:18:47 +0700

    tonghieutichthuong

---
+++ TongHieuTichThuong_tamplate.cpp
@@ -0,0 +1,32 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+using
+if
+switch
+while
+for
+break
+###End banned keyword*/
+#include <iostream>
+
+using namespace std;
+
+//###INSERT CODE HERE -
+int main()
+{
+	int a;
+	int b;
+	cout<<"Nhap so a :"<<endl;
+	cin>>a;
+	cout<<"Nhap so b :"<<endl;
+	cin>>b;
+	cout<<"Tong 2 so a va b :"<<a+b <<endl;
+	cout<<"Hieu 2 so a va b :"<<a-b<<endl;
+	cout<<"Tich hai so a va b:"<<a*b<<endl;
+	cout<<"Thuong 2 so a va b :"<<a/b<<endl;
+	return 0;
+	
+}
+
+
