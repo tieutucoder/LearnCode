525cf4e86f5c799b3409cae3b5137e5ef8c407a3
chaykhongduoc <71576097+chaykhongduoc@users.noreply.github.com>
2020-10-20 18:16:57 +0700

    Update Lapvotan.cpp.cpp

--- BTVN2/Lapvotan.cpp.cpp
+++ BTVN2/Lapvotan.cpp.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 
 int main(){
-    long a,b;
+    long a,b;   
     cin >> a >> b;
-    if((a%2!=0 && b%2==0) || (b%2!=0 && a%2==0))
+    if((a%2!=0 && b%2==0) || (b%2!=0 && a%2==0) || a>b)
         cout << "TRUE";
     else
         cout << "FALSE";
 }
-
